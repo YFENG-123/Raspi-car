35ecac0e054be5af42ff012f2d81871774b0ccd6
YFENG-123 <578851062@qq.com>
2025-01-28 05:58:40 +0800

    Update picoPWM

--- MicroPython/app/main.py
+++
@@ -1,5 +0,0 @@
-import machine
-p12 = machine.Pin(12)
-pwm12 = machine.PWM(p12)
-pwm12.freq(50)
-pwm12.duty(512)
---
+++ pico2w/uart_demo.py
@@ -0,0 +1,5 @@
+from machine import UART, Pin
+uart1 = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+uart1.write('hello')  # write 5 bytes
+buf = uart1.read(5)         # read up to 5 bytes
+print(buf)
