c0f2700446fc3398668f36fb6f07c91d5d8f97f5
YFENG-123 <578851062@qq.com>
2025-02-03 20:55:46 +0800

    Remove dir micropython

--- MicroPython/micropython_demo/freq_demo.py
+++
@@ -1,5 +0,0 @@
-#MicroPython REPL 通过 USB 串行端口访问。制表符完成对于找出对象具有哪些方法很有用。粘贴模式 (ctrl-E) 可用于将大量 Python 代码粘贴到 REPL 中。
-import machine
-
-machine.freq()          # get the current frequency of the CPU
-machine.freq(240000000) # set the CPU frequency to 240 MHz
--- MicroPython/micropython_demo/wdt_demo.py
+++
@@ -1,9 +0,0 @@
-# RP2040 有一个看门狗，它是一个倒数计时器，可以在它达到零时重新启动芯片的某些部分。
-# 参见 machine.WDT http://micropython.com.cn/en/latet/library/machine.WDT.html#machine-wdt
-
-from machine import WDT
-
-# enable the WDT with a timeout of 5s (1s is the minimum)
-wdt = WDT(timeout=5000)
-wdt.feed()
-
